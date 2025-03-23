dfcf7a35cdc3dc199e9b2685d538e1c07e810d81
Thiago Lopes <thslopes@gmail.com>
2025-03-23 17:07:28 -0300

    refac

--- utils/styles.gtr3.mini.js
+++ utils/styles.gtr3.mini.js
@@ -3,7 +3,58 @@ import zui from '@zos/ui';
 
 const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = zdevice.getDeviceInfo();
 
-const { width: calorieLabelWidth, height: calorieLabelHeight } = zui.getTextLayout('cal', {
+const startButtonWidth = 200;
+const startButtonHeight = 100;
+const startButtonX = DEVICE_WIDTH / 2 - startButtonWidth / 2;
+
+export const START_BUTTON = {
+    x: startButtonX,
+    y: 0,
+    w: startButtonWidth,
+    h: startButtonHeight,
+    radius: 12,
+    text_size: 36,
+    color: 0xffffff,
+    align_h: zui.align.CENTER_H,
+    align_v: zui.align.CENTER_V,
+    normal_color: 0x00dd00,
+    press_color: 0xff0000
+}
+
+const { width: maxHRTextWidth, height: maxHRTextHeight } = zui.getTextLayout('0000', {
+    text_size: 36,
+    text_width: 200
+})
+
+const heartRateTextX = DEVICE_WIDTH / 2 + 5;
+const heartRateTextY = startButtonHeight + 20;
+
+export const HEART_RATE_TEXT = {
+    x: heartRateTextX,
+    y: heartRateTextY,
+    w: maxHRTextWidth,
+    h: maxHRTextHeight,
+    color: 0xffffff,
+    text_size: 36,
+    align_h: zui.align.LEFT,
+    align_v: zui.align.CENTER_V,
+    text_style: zui.text_style.NONE
+}
+
+export const HR_LABEL = {
+    x: 0,
+    y: heartRateTextY,
+    w: DEVICE_WIDTH / 2 - 5,
+    h: maxHRTextHeight,
+    color: 0xffffff,
+    text_size: 36,
+    align_h: zui.align.RIGHT,
+    align_v: zui.align.CENTER_V,
+    text_style: zui.text_style.NONE
+}
+
+
+const { height: calorieLabelHeight } = zui.getTextLayout('cal', {
     text_size: 36,
     text_width: 200
 })
@@ -13,7 +64,7 @@ const { width: maxCalTextWidth, height: maxCalTextHeight } = zui.getTextLayout('
     text_width: 200
 })
 
-const calorieLabelY = DEVICE_HEIGHT / 2 - 23 + 46;
+const calorieLabelY = heartRateTextY + maxHRTextHeight;
 
 export const CALORIE_LABEL = {
     x: 0,
@@ -40,55 +91,4 @@ export const CALORIE_TEXT = {
     align_h: zui.align.LEFT,
     align_v: zui.align.CENTER_V,
     text_style: zui.text_style.NONE
-}
-
-const { width: maxHRTextWidth, height: maxHRTextHeight } = zui.getTextLayout('0000', {
-    text_size: 36,
-    text_width: 200
-})
-
-const heartRateTextX = DEVICE_WIDTH / 2 + 5;
-const heartRateTextY = DEVICE_HEIGHT / 2 - 23;
-
-export const HEART_RATE_TEXT = {
-    x: heartRateTextX,
-    y: heartRateTextY,
-    w: maxHRTextWidth,
-    h: maxHRTextHeight,
-    color: 0xffffff,
-    text_size: 36,
-    align_h: zui.align.LEFT,
-    align_v: zui.align.CENTER_V,
-    text_style: zui.text_style.NONE
-}
-
-export const HR_LABEL = {
-    x: 0,
-    y: heartRateTextY,
-    w: DEVICE_WIDTH / 2 - 5,
-    h: calorieLabelHeight,
-    color: 0xffffff,
-    text_size: 36,
-    align_h: zui.align.RIGHT,
-    align_v: zui.align.CENTER_V,
-    text_style: zui.text_style.NONE
-}
-
-
-const startButtonWidth = 200;
-const startButtonHeight = 100;
-const startButtonX = DEVICE_WIDTH / 2 - startButtonWidth / 2;
-
-export const START_BUTTON = {
-    x: startButtonX,
-    y: 0,
-    w: startButtonWidth,
-    h: startButtonHeight,
-    radius: 12,
-    text_size: 36,
-    color: 0xffffff,
-    align_h: zui.align.CENTER_H,
-    align_v: zui.align.CENTER_V,
-    normal_color: 0x00dd00,
-    press_color: 0xff0000
 }
