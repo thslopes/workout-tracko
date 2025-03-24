476227f26e68286adb6b94bc43426bc8d27bb519
Thiago Lopes <thslopes@gmail.com>
2025-03-23 23:20:25 -0300

    redesign

--- page/index.js
+++ page/index.js
@@ -1,7 +1,7 @@
 import { getText } from '@zos/i18n'
 import { Calorie, HeartRate, Time } from '@zos/sensor'
 import { createWidget, prop, widget } from '@zos/ui'
-import { CALORIE_LABEL, CALORIE_TEXT, HEART_RATE_TEXT, HR_LABEL, START_BUTTON, TIME_TEXT, TOTAL_TIME } from '../utils/styles.gtr3.mini'
+import { CALORIE_LABEL, CALORIE_TEXT, EXERCISE_TEXT, FINISH_BUTTON, HEART_RATE_TEXT, HR_LABEL, SET_TEXT, START_BUTTON, TIME_TEXT, TOTAL_TIME } from '../utils/styles.gtr3.mini'
 
 
 Page({
@@ -38,6 +38,16 @@ Page({
       text: 0
     })
 
+    const setText = createWidget(widget.TEXT, {
+      ...SET_TEXT,
+      text: '1 / 3'
+    })
+
+    const exerciseText = createWidget(widget.TEXT, {
+      ...EXERCISE_TEXT,
+      text: 'Agachamento Máquina com barra'
+    })
+
     const calorieChangeCallback = () => {
       calorieText.setProperty(prop.TEXT, {
         text: calorie.getCurrent() - this.state.initialCalorie,
@@ -60,19 +70,22 @@ Page({
       click_func: (btn) => {
         if (this.state.startTime === 0) {
           this.state.startTime = new Time().getTime()
-          setInterval(setIntervalTime(this.state.startTime, totalTimeTimer), 100)
-        }
-
-        if (this.state.started) {
-          this.state.started = false
-          btn.setProperty(prop.TEXT, { text: 'Start' })
-          clearInterval(this.state.timerId)
-        } else {
-          this.state.started = true
           this.state.intervalTime = new Time().getTime()
-          btn.setProperty(prop.TEXT, { text: 'Stop' })
+          setInterval(setIntervalTime(this.state.startTime, totalTimeTimer), 100)
           this.state.timerId = setInterval(setIntervalTime(this.state.intervalTime, intervalTimer), 100)
+          this.state.started = true
+          btn.setProperty(prop.TEXT, { text: 'Stop' })
         }
+      }
+    })
+
+    createWidget(widget.BUTTON, {
+      ...FINISH_BUTTON,
+      text: 'Rest',
+      click_func: () => {
+        clearInterval(this.state.timerId)
+        this.state.intervalTime = new Time().getTime()
+        this.state.timerId = setInterval(setIntervalTime(this.state.intervalTime, intervalTimer), 100)
       }
     })
 
@@ -90,19 +103,19 @@ Page({
 
 const setIntervalTime = (startTime, component) => {
   return () => {
-      const currentTime = new Time().getTime()
-      const elapsedTime = currentTime - startTime
-      const elapsedHours = Math.floor(elapsedTime / 1000 / 60 / 60)
-      const elapsedMinutes = Math.floor(elapsedTime / 1000 / 60) % 60
-      const elapsedSeconds = Math.floor(elapsedTime / 1000) % 60
-      const elapsedMilliseconds = Math.floor(elapsedTime / 100) % 10
-      let formattedTime = `${String(elapsedMinutes).padStart(2, '0')}:${String(elapsedSeconds).padStart(2, '0')}.${elapsedMilliseconds}`
-      if (elapsedHours > 0) {
-          formattedTime = `${String(elapsedHours).padStart(2, '0')}:${formattedTime}`
-      }
-      component.setProperty(prop.TEXT, {
-          text: formattedTime
-      })
+    const currentTime = new Time().getTime()
+    const elapsedTime = currentTime - startTime
+    const elapsedHours = Math.floor(elapsedTime / 1000 / 60 / 60)
+    const elapsedMinutes = Math.floor(elapsedTime / 1000 / 60) % 60
+    const elapsedSeconds = Math.floor(elapsedTime / 1000) % 60
+    const elapsedMilliseconds = Math.floor(elapsedTime / 100) % 10
+    let formattedTime = `${String(elapsedMinutes).padStart(2, '0')}:${String(elapsedSeconds).padStart(2, '0')}.${elapsedMilliseconds}`
+    if (elapsedHours > 0) {
+      formattedTime = `${String(elapsedHours).padStart(2, '0')}:${formattedTime}`
+    }
+    component.setProperty(prop.TEXT, {
+      text: formattedTime
+    })
   }
 }
 
--- utils/styles.gtr3.mini.js
+++ utils/styles.gtr3.mini.js
@@ -4,7 +4,7 @@ import zui from '@zos/ui';
 const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = zdevice.getDeviceInfo();
 
 const startButtonWidth = 200;
-const startButtonHeight = 100;
+const startButtonHeight = 80;
 const startButtonX = DEVICE_WIDTH / 2 - startButtonWidth / 2;
 
 export const START_BUTTON = {
@@ -13,7 +13,7 @@ export const START_BUTTON = {
     w: startButtonWidth,
     h: startButtonHeight,
     radius: 12,
-    text_size: 36,
+    text_size: 30,
     color: 0xffffff,
     align_h: zui.align.CENTER_H,
     align_v: zui.align.CENTER_V,
@@ -21,84 +21,128 @@ export const START_BUTTON = {
     press_color: 0xff0000
 }
 
-const { width: maxHRTextWidth, height: maxHRTextHeight } = zui.getTextLayout('0000', {
-    text_size: 36,
+const finishButtonWidth = 200;
+const finishButtonHeight = 80;
+const finishButtonX = DEVICE_WIDTH / 2 - finishButtonWidth / 2;
+const finishButtonY = DEVICE_HEIGHT - finishButtonHeight;
+
+export const FINISH_BUTTON = {
+    x: finishButtonX,
+    y: finishButtonY,
+    w: finishButtonWidth,
+    h: finishButtonHeight,
+    radius: 12,
+    text_size: 30,
+    color: 0x000000,
+    align_h: zui.align.CENTER_H,
+    align_v: zui.align.TOP,
+    normal_color: 0xffffa0,
+    press_color: 0xff0000
+}
+
+const { height: hrCalorieLabelHeight } = zui.getTextLayout('cal', {
+    text_size: 18,
     text_width: 200
 })
 
-const heartRateTextX = DEVICE_WIDTH / 2 + 5;
-const heartRateTextY = startButtonHeight + 5;
+const hrCaloriesLabelY = startButtonHeight + 5;
 
 export const HR_LABEL = {
-    x: 0,
-    y: heartRateTextY,
-    w: DEVICE_WIDTH / 2 - 5,
-    h: maxHRTextHeight,
+    x: DEVICE_WIDTH / 2 - 100,
+    y: hrCaloriesLabelY,
+    w: 100,
+    h: hrCalorieLabelHeight,
     color: 0xffffff,
-    text_size: 36,
-    align_h: zui.align.RIGHT,
+    text_size: 18,
+    align_h: zui.align.CENTER_H,
     align_v: zui.align.CENTER_V,
     text_style: zui.text_style.NONE
 }
+
+export const CALORIE_LABEL = {
+    x: DEVICE_WIDTH / 2,
+    y: hrCaloriesLabelY,
+    w: 100,
+    h: hrCalorieLabelHeight,
+    color: 0xffffff,
+    text_size: 18,
+    align_h: zui.align.CENTER_H,
+    align_v: zui.align.CENTER_V,
+    text_style: zui.text_style.NONE
+}
+
+const { height: maxHRCalorieTextHeight } = zui.getTextLayout('0000', {
+    text_size: 30,
+    text_width: 200
+})
+
+const hrCaloriesTextY = hrCaloriesLabelY + hrCalorieLabelHeight;
+
+const heartRateTextX = DEVICE_WIDTH / 2 - 100;
 
 export const HEART_RATE_TEXT = {
     x: heartRateTextX,
-    y: heartRateTextY,
-    w: maxHRTextWidth,
-    h: maxHRTextHeight,
+    y: hrCaloriesTextY,
+    w: 100,
+    h: maxHRCalorieTextHeight,
     color: 0xffffff,
-    text_size: 36,
-    align_h: zui.align.LEFT,
+    text_size: 30,
+    align_h: zui.align.CENTER_H,
     align_v: zui.align.CENTER_V,
     text_style: zui.text_style.NONE
 }
 
-const { height: calorieLabelHeight } = zui.getTextLayout('cal', {
-    text_size: 36,
-    text_width: 200
-})
-
-const { width: maxCalTextWidth, height: maxCalTextHeight } = zui.getTextLayout('0000', {
-    text_size: 36,
-    text_width: 200
-})
-
-const calorieLabelY = heartRateTextY + maxHRTextHeight;
-
-export const CALORIE_LABEL = {
-    x: 0,
-    y: calorieLabelY,
-    w: DEVICE_WIDTH / 2 - 5,
-    h: calorieLabelHeight,
-    color: 0xffffff,
-    text_size: 36,
-    align_h: zui.align.RIGHT,
-    align_v: zui.align.CENTER_V,
-    text_style: zui.text_style.NONE
-}
-
-const calorieTextX = DEVICE_WIDTH / 2 + 5;
-const calorieTextY = calorieLabelY;
+const calorieTextX = DEVICE_WIDTH / 2;
 
 export const CALORIE_TEXT = {
     x: calorieTextX,
-    y: calorieTextY,
-    w: maxCalTextWidth,
-    h: maxCalTextHeight,
+    y: hrCaloriesTextY,
+    w: 100,
+    h: maxHRCalorieTextHeight,
     color: 0xffffff,
-    text_size: 36,
-    align_h: zui.align.LEFT,
+    text_size: 30,
+    align_h: zui.align.CENTER_H,
     align_v: zui.align.CENTER_V,
     text_style: zui.text_style.NONE
 }
 
+export const SET_TEXT = {
+    x: 0,
+    y: hrCaloriesTextY + 20,
+    w: DEVICE_WIDTH,
+    h: startButtonHeight,
+    color: 0xffffff,
+    text_size: 18,
+    align_h: zui.align.CENTER_H,
+    align_v: zui.align.CENTER_V,
+    text_style: zui.text_style.NONE
+}
+
+export const EXERCISE_TEXT = {
+    x: 0,
+    y: DEVICE_HEIGHT / 2 - maxHRCalorieTextHeight / 2,
+    w: DEVICE_WIDTH,
+    h: startButtonHeight,
+    color: 0xffffff,
+    text_size: 30,
+    align_h: zui.align.CENTER_H,
+    align_v: zui.align.CENTER_V,
+    text_style: zui.text_style.WRAP
+}
+
 const { width: timeTextWitdh, height: timeTextHeight } = zui.getTextLayout('00:00:00.0', {
-    text_size: 36,
+    text_size: 30,
+    text_width: 200
+})
+
+const { height: totalTimeTextHeight } = zui.getTextLayout('00:00:00.0', {
+    text_size: 18,
     text_width: 200
 })
 
 const timeTextX = DEVICE_WIDTH / 2 - timeTextWitdh / 2;
-const timeTextY = calorieLabelY + calorieLabelHeight + 20;
+const totalTimeTextY = DEVICE_HEIGHT - finishButtonHeight - totalTimeTextHeight - 20;
+const timeTextY = totalTimeTextY - 30;
 
 export const TIME_TEXT = {
     x: timeTextX,
@@ -106,13 +150,11 @@ export const TIME_TEXT = {
     w: timeTextWitdh,
     h: timeTextHeight,
     color: 0xffffff,
-    text_size: 36,
+    text_size: 30,
     align_h: zui.align.CENTER_H,
     align_v: zui.align.CENTER_V,
     text_style: zui.text_style.NONE
 }
-
-const totalTimeTextY = timeTextY + timeTextHeight + 20;
 
 export const TOTAL_TIME = {
     x: timeTextX,
@@ -120,7 +162,7 @@ export const TOTAL_TIME = {
     w: timeTextWitdh,
     h: timeTextHeight,
     color: 0xffffff,
-    text_size: 36,
+    text_size: 18,
     align_h: zui.align.CENTER_H,
     align_v: zui.align.CENTER_V,
     text_style: zui.text_style.NONE
