86c81e1d55f9c34621117a2d76006629b8a54d94
Thiago Lopes <thslopes@gmail.com>
2025-03-27 09:42:27 -0300

    Die Hard

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+import { setWakeUpRelaunch } from '@zos/display'
 import { setStatusBarVisible, updateStatusBarTitle } from '@zos/ui'
 
 App({
   onCreate(options) {
     console.log('app on create invoke')
-    const title = 'Mini Program Title'
+    const title = 'Tracko'
 
     updateStatusBarTitle(title)
     setStatusBarVisible(true)
+    setWakeUpRelaunch({
+      relaunch: true,
+    })
   },
 
   onDestroy(options) {
