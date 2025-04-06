3c649bc3c55cdff4e2d995ea9533cc0cb7dcacf3
Thiago Lopes <thslopes@gmail.com>
2025-04-06 15:37:01 -0300

    refac

--- page/doing.js
+++ page/doing.js
@@ -1,156 +1,19 @@
-import { getText } from '@zos/i18n'
+import { setPageBrightTime } from '@zos/display'
 import { push, replace } from '@zos/router'
-import { Calorie, Distance, HeartRate, Step, Time, Vibrator, VIBRATOR_SCENE_STRONG_REMINDER } from '@zos/sensor'
-import { sessionStorage } from '@zos/storage'
-import { createWidget, prop, widget } from '@zos/ui'
+import { Vibrator, VIBRATOR_SCENE_STRONG_REMINDER } from '@zos/sensor'
+import { createWidget, widget } from '@zos/ui'
 import { getActualSet } from '../utils/exercise'
 import * as styles from './workout.styles'
-import { setPageBrightTime } from '@zos/display'
+import WorkoutBase from './workoutBase'
 
+const workoutBase = new WorkoutBase()
 
 Page({
-  state: {
-    startTime: 0,
-    intervalTime: 0,
-    timerId: null,
-    initialCalorie: 0,
-    initialDistance: 0,
-    initialSteps: 0,
-    actualSet: 1,
-    actualExercise: 0,
-    actualRest: 0
-  },
-  loadState() {
-    this.state.workout = JSON.parse(sessionStorage.getItem('workout'))
-    this.state.startTime = sessionStorage.getItem('startTime', 0)
-    if (!this.state.startTime) {
-      this.state.startTime = new Time().getTime()
-      sessionStorage.setItem('startTime', this.state.startTime)
-    }
-
-    this.state.initialCalorie = sessionStorage.getItem('initialCalorie')
-    this.state.initialDistance = sessionStorage.getItem('initialDistance')
-    this.state.initialSteps = sessionStorage.getItem('initialSteps')
-
-    this.state.actualSet = sessionStorage.getItem('actualSet')
-    if (!this.state.actualSet) {
-      this.state.actualSet = 1
-    }
-
-    this.state.actualExercise = sessionStorage.getItem('actualExercise')
-    if (!this.state.actualExercise) {
-      this.state.actualExercise = 0
-    }
-
-    console.log('startTime', this.state.startTime)
-    console.log('initialCalorie', this.state.initialCalorie)
-    console.log('actualSet', this.state.actualSet)
-    console.log('actualExercise', this.state.actualExercise)
-  },
-  saveState() {
-    sessionStorage.setItem('actualSet', this.state.actualSet)
-    sessionStorage.setItem('actualExercise', this.state.actualExercise)
-    sessionStorage.setItem('startTime', this.state.startTime)
-    sessionStorage.setItem('initialCalorie', this.state.initialCalorie)
-    sessionStorage.setItem('initialDistance', this.state.initialDistance)
-    sessionStorage.setItem('initialSteps', this.state.initialSteps)
-    sessionStorage.setItem('rest', this.state.rest)
-  },
+  state: workoutBase.state,
+  loadState: workoutBase.loadState,
+  saveState: workoutBase.saveState,
   build() {
-    this.loadState()
-    const heartRate = new HeartRate()
-    const calorie = new Calorie()
-    const distance = new Distance()
-    const step = new Step()
-
-    if (!this.state.initialCalorie) {
-      this.state.initialCalorie = calorie.getCurrent()
-    }
-
-    if (!this.state.initialDistance) {
-      this.state.initialDistance = distance.getCurrent()
-    }
-
-    if (!this.state.initialSteps) {
-      this.state.initialSteps = step.getCurrent()
-    }
-
-    createWidget(widget.TEXT, {
-      ...styles.HR_LABEL,
-      text: getText('hr')
-    })
-
-    const hrText = createWidget(widget.TEXT, {
-      ...styles.HEART_RATE_TEXT,
-      text: heartRate.getCurrent()
-    })
-
-    createWidget(widget.TEXT, {
-      ...styles.CALORIE_LABEL,
-      text: getText('cal')
-    })
-
-    const calorieText = createWidget(widget.TEXT, {
-      ...styles.CALORIE_TEXT,
-      text: calorie.getCurrent() - this.state.initialCalorie
-    })
-
-    if (this.state.workout[this.state.actualExercise].sets > 0) {
-      const setText = createWidget(widget.TEXT, {
-        ...styles.SET_TEXT,
-        text: `${this.state.actualSet} / ${this.state.workout[this.state.actualExercise].sets}`
-      })
-    }
-
-    const exerciseText = createWidget(widget.TEXT, {
-      ...styles.EXERCISE_TEXT,
-      text: this.state.workout[this.state.actualExercise].name
-    })
-
-    this.state.rest = this.state.workout[this.state.actualExercise].rest
-
-    const calorieChangeCallback = () => {
-      calorieText.setProperty(prop.TEXT, {
-        text: calorie.getCurrent() - this.state.initialCalorie,
-      });
-    };
-
-    calorie.onChange(calorieChangeCallback)
-
-    const hrChangeCallback = () => {
-      if (!sessionStorage.getItem('hrData')) {
-        sessionStorage.setItem('hrData', JSON.stringify([]))
-      }
-      const hrData = JSON.parse(sessionStorage.getItem('hrData'))
-      const currentTime = new Time().getTime()
-      const currentHr = heartRate.getCurrent()
-
-      hrData.push({
-        time: currentTime,
-        hr: currentHr
-      })
-
-      sessionStorage.setItem('hrData', JSON.stringify(hrData))
-
-      hrText.setProperty(prop.TEXT, {
-        text: currentHr,
-      });
-    };
-
-    heartRate.onCurrentChange(hrChangeCallback)
-
-    createWidget(widget.BUTTON, {
-      ...styles.FINISH_BUTTON,
-      text: 'FINISH',
-      click_func: () => {
-        sessionStorage.setItem('endTime', new Time().getTime())
-        sessionStorage.setItem('startTime', this.state.startTime)
-        sessionStorage.setItem('totalCalorie', calorie.getCurrent() - this.state.initialCalorie)
-        sessionStorage.setItem('totalDistance', distance.getCurrent() - this.state.initialDistance)
-        sessionStorage.setItem('totalSteps', step.getCurrent() - this.state.initialSteps)
-        replace({ url: 'page/resume', params: '' })
-      }
-    })
+    workoutBase.baseBuild()
 
     const shouldDisplayRestButton = this.state.workout[this.state.actualExercise].sets > this.state.actualSet || this.state.workout.length > this.state.actualExercise + 1
     if (shouldDisplayRestButton) {
@@ -175,56 +38,6 @@ Page({
         }
       })
     }
-
-    const intervalTimer = createWidget(widget.TEXT, {
-      ...styles.TIME_TEXT,
-      text: '00:00:00.0'
-    })
-
-    const totalTimeTimer = createWidget(widget.TEXT, {
-      ...styles.TOTAL_TIME,
-      text: '00:00:00.0'
-    })
-
-    if (sessionStorage.getItem('external', 0)) {
-
-      const stepCount = createWidget(widget.TEXT, {
-        ...styles.STEPS_TEXT,
-        text: step.getCurrent() - this.state.initialSteps
-      })
-      const distanceCount = createWidget(widget.TEXT, {
-        ...styles.DISTANCE_TEXT,
-        text: distance.getCurrent() - this.state.initialDistance
-      })
-
-      createWidget(widget.TEXT, {
-        ...styles.STEPS_LABEL,
-        text: 'steps'
-      })
-      createWidget(widget.TEXT, {
-        ...styles.DISTANCE_LABEL,
-        text: 'distance'
-      })
-
-      const stepCountChangeCallback = () => {
-        stepCount.setProperty(prop.TEXT, {
-          text: step.getCurrent() - this.state.initialSteps
-        });
-      };
-      step.onChange(stepCountChangeCallback)
-
-      const distanceCountChangeCallback = () => {
-        distanceCount.setProperty(prop.TEXT, {
-          text: distance.getCurrent() - this.state.initialDistance
-        });
-      };
-      distance.onChange(distanceCountChangeCallback)
-    }
-
-    console.log('startTime', this.state.startTime)
-    this.state.intervalTime = new Time().getTime()
-    setInterval(setIntervalTime(this.state.startTime, totalTimeTimer), 100)
-    this.state.timerId = setInterval(setIntervalTime(this.state.intervalTime, intervalTimer), 100)
 
     if (this.state.workout[this.state.actualExercise].duration) {
       const result = setPageBrightTime({
@@ -251,22 +64,3 @@ Page({
     }
   }
 })
-
-const setIntervalTime = (startTime, component) => {
-  return () => {
-    const currentTime = new Time().getTime()
-    const elapsedTime = currentTime - startTime
-    const elapsedHours = Math.floor(elapsedTime / 1000 / 60 / 60)
-    const elapsedMinutes = Math.floor(elapsedTime / 1000 / 60) % 60
-    const elapsedSeconds = Math.floor(elapsedTime / 1000) % 60
-    const elapsedMilliseconds = Math.floor(elapsedTime / 100) % 10
-    let formattedTime = `${String(elapsedMinutes).padStart(2, '0')}:${String(elapsedSeconds).padStart(2, '0')}.${elapsedMilliseconds}`
-    if (elapsedHours > 0) {
-      formattedTime = `${String(elapsedHours).padStart(2, '0')}:${formattedTime}`
-    }
-    component.setProperty(prop.TEXT, {
-      text: formattedTime
-    })
-  }
-}
-
--- page/rest.js
+++ page/rest.js
@@ -1,151 +1,20 @@
-import { getText } from '@zos/i18n'
-import { back, replace } from '@zos/router'
-import { Calorie, Distance, HeartRate, Step, Time, Vibrator, VIBRATOR_SCENE_STRONG_REMINDER } from '@zos/sensor'
-import { sessionStorage } from '@zos/storage'
-import { createWidget, prop, widget } from '@zos/ui'
-import * as styles from './workout.styles'
 import { setPageBrightTime } from '@zos/display'
+import { back } from '@zos/router'
+import { Vibrator, VIBRATOR_SCENE_STRONG_REMINDER } from '@zos/sensor'
+import { sessionStorage } from '@zos/storage'
+import { createWidget, widget } from '@zos/ui'
+import * as styles from './workout.styles'
+import WorkoutBase from './workoutBase'
 
+const workoutBase = new WorkoutBase()
 
 Page({
-  state: {
-    startTime: 0,
-    intervalTime: 0,
-    timerId: null,
-    initialCalorie: 0,
-    initialDistance: 0,
-    initialSteps: 0,
-    actualSet: 1,
-    actualExercise: 0
-  },
-  loadState() {
-    this.state.workout = JSON.parse(sessionStorage.getItem('workout'))
-    this.state.startTime = sessionStorage.getItem('startTime', 0)
-    this.state.initialCalorie = sessionStorage.getItem('initialCalorie', 0)
-    this.state.initialDistance = sessionStorage.getItem('initialDistance', 0)
-    this.state.initialSteps = sessionStorage.getItem('initialSteps', 0)
-    this.state.actualSet = sessionStorage.getItem('actualSet', 1)
-    this.state.actualExercise = sessionStorage.getItem('actualExercise', 0)
-    console.log('startTime', this.state.startTime)
-    console.log('initialCalorie', this.state.initialCalorie)
-    console.log('actualSet', this.state.actualSet)
-    console.log('actualExercise', this.state.actualExercise)
-  },
+  state: workoutBase.state,
+  loadState: workoutBase.loadState,
   build() {
-    this.loadState()
-    const heartRate = new HeartRate()
-    const calorie = new Calorie()
-    const distance = new Distance()
-    const step = new Step()
+    workoutBase.baseBuild()
 
-    createWidget(widget.TEXT, {
-      ...styles.HR_LABEL,
-      text: getText('hr')
-    })
-
-    const hrText = createWidget(widget.TEXT, {
-      ...styles.HEART_RATE_TEXT,
-      text: heartRate.getCurrent()
-    })
-
-    createWidget(widget.TEXT, {
-      ...styles.CALORIE_LABEL,
-      text: getText('cal')
-    })
-
-    if (sessionStorage.getItem('external', 0)) {
-
-      const stepCount = createWidget(widget.TEXT, {
-        ...styles.STEPS_TEXT,
-        text: step.getCurrent() - this.state.initialSteps
-      })
-      const distanceCount = createWidget(widget.TEXT, {
-        ...styles.DISTANCE_TEXT,
-        text: distance.getCurrent() - this.state.initialDistance
-      })
-
-      createWidget(widget.TEXT, {
-        ...styles.STEPS_LABEL,
-        text: 'steps'
-      })
-      createWidget(widget.TEXT, {
-        ...styles.DISTANCE_LABEL,
-        text: 'distance'
-      })
-
-      const stepCountChangeCallback = () => {
-        stepCount.setProperty(prop.TEXT, {
-          text: step.getCurrent() - this.state.initialSteps
-        });
-      };
-      step.onChange(stepCountChangeCallback)
-
-      const distanceCountChangeCallback = () => {
-        distanceCount.setProperty(prop.TEXT, {
-          text: distance.getCurrent() - this.state.initialDistance
-        });
-      };
-      distance.onChange(distanceCountChangeCallback)
-    }
-
-
-    const calorieText = createWidget(widget.TEXT, {
-      ...styles.CALORIE_TEXT,
-      text: calorie.getCurrent() - this.state.initialCalorie
-    })
-
-    const setText = createWidget(widget.TEXT, {
-      ...styles.SET_TEXT,
-      text: `${this.state.actualSet} / ${this.state.workout[this.state.actualExercise].sets}`
-    })
-
-    const exerciseText = createWidget(widget.TEXT, {
-      ...styles.EXERCISE_TEXT,
-      text: this.state.workout[this.state.actualExercise].name
-    })
-
-    const calorieChangeCallback = () => {
-      calorieText.setProperty(prop.TEXT, {
-        text: calorie.getCurrent() - this.state.initialCalorie,
-      });
-    };
-
-    calorie.onChange(calorieChangeCallback)
-
-    const hrChangeCallback = () => {
-      if (!sessionStorage.getItem('hrData')) {
-        sessionStorage.setItem('hrData', JSON.stringify([]))
-      }
-      const hrData = JSON.parse(sessionStorage.getItem('hrData'))
-      const currentTime = new Time().getTime()
-      const currentHr = heartRate.getCurrent()
-
-      hrData.push({
-        time: currentTime,
-        hr: currentHr
-      })
-
-      sessionStorage.setItem('hrData', JSON.stringify(hrData))
-
-      hrText.setProperty(prop.TEXT, {
-        text: heartRate.getCurrent(),
-      });
-    };
-
-    heartRate.onCurrentChange(hrChangeCallback)
-
-    createWidget(widget.BUTTON, {
-      ...styles.FINISH_BUTTON,
-      text: 'FINISH',
-      click_func: () => {
-        sessionStorage.setItem('endTime', new Time().getTime())
-        sessionStorage.setItem('startTime', this.state.startTime)
-        sessionStorage.setItem('totalCalorie', calorie.getCurrent() - this.state.initialCalorie)
-        sessionStorage.setItem('totalDistance', distance.getCurrent() - this.state.initialDistance)
-        sessionStorage.setItem('totalSteps', step.getCurrent() - this.state.initialSteps)
-        replace({ url: 'page/resume', params: '' })
-      }
-    })
+    const restTime = sessionStorage.getItem('rest', 60)
 
     createWidget(widget.BUTTON, {
       ...styles.GO_BUTTON,
@@ -155,21 +24,6 @@ Page({
       }
     })
 
-    const intervalTimer = createWidget(widget.TEXT, {
-      ...styles.TIME_TEXT,
-      text: '00:00:00.0'
-    })
-
-    const totalTimeTimer = createWidget(widget.TEXT, {
-      ...styles.TOTAL_TIME,
-      text: '00:00:00.0'
-    })
-
-    this.state.intervalTime = new Time().getTime()
-    setInterval(setIntervalTime(this.state.startTime, totalTimeTimer), 100)
-    this.state.timerId = setInterval(setIntervalTime(this.state.intervalTime, intervalTimer), 100)
-
-    const restTime = sessionStorage.getItem('rest', 60)
     const result = setPageBrightTime({
       brightTime: 1000 * restTime,
     })
@@ -185,22 +39,3 @@ Page({
     }, 1000 * (restTime - 3))
   }
 })
-
-const setIntervalTime = (startTime, component) => {
-  return () => {
-    const currentTime = new Time().getTime()
-    const elapsedTime = currentTime - startTime
-    const elapsedHours = Math.floor(elapsedTime / 1000 / 60 / 60)
-    const elapsedMinutes = Math.floor(elapsedTime / 1000 / 60) % 60
-    const elapsedSeconds = Math.floor(elapsedTime / 1000) % 60
-    const elapsedMilliseconds = Math.floor(elapsedTime / 100) % 10
-    let formattedTime = `${String(elapsedMinutes).padStart(2, '0')}:${String(elapsedSeconds).padStart(2, '0')}.${elapsedMilliseconds}`
-    if (elapsedHours > 0) {
-      formattedTime = `${String(elapsedHours).padStart(2, '0')}:${formattedTime}`
-    }
-    component.setProperty(prop.TEXT, {
-      text: formattedTime
-    })
-  }
-}
-
